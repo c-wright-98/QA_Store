b947e48d0f96cfe09dd5a6a22dbbde43a5ce875b
c-wright-98 <corvus.wright@gmail.com>
2023-08-11 13:12:59 +0100

    Added basic flask code

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template,request,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from flask_testing import TestCase
+from flask_bcrypt import Bcrypt
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SECRET_KEY'] = "12736453"
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
