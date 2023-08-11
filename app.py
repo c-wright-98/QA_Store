07d849137191ee27f51b013cb59173afd21946be
c-wright-98 <corvus.wright@gmail.com>
2023-08-11 13:28:42 +0100

    Added test text to check site

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to the home page"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
