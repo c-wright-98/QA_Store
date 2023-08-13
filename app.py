3c888715b95c94636f3b2d3817afe43b86bc506c
c-wright-98 <corvus.wright@gmail.com>
2023-08-13 22:03:29 +0100

    Added footer styling, footer pages and links

--- app.py
+++ app.py
@@ -42,6 +42,14 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/contact-us', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
