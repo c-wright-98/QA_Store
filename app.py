4de83095085dd2752cb0325e228ecb5149573278
c-wright-98 <corvus.wright@gmail.com>
2023-08-13 21:10:02 +0100

    Moved images to static, adjusted style for home link

--- app.py
+++ app.py
@@ -34,6 +34,14 @@ def categories():
 def cart():
     return render_template('cart.html')
 
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
