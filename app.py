78adc78a8b1e4cb54b57c8d5104f009e90c36351
c-wright-98 <corvus.wright@gmail.com>
2023-08-14 22:44:30 +0100

    Added post, added html forms, added payment and checkout

--- app.py
+++ app.py
@@ -14,42 +14,49 @@ db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('home.html')
 
-@app.route('/orders', methods=['GET'])
+@app.route('/orders', methods=['GET','POST'])
 def order_history():
     return render_template('orders.html')
 
-@app.route('/products', methods=['GET'])
+@app.route('/products', methods=['GET','POST'])
 def products():
     return render_template('products.html')
 
-@app.route('/categories', methods=['GET'])
+@app.route('/categories', methods=['GET','POST'])
 def categories():
     return render_template('categories.html')
 
-@app.route('/cart', methods=['GET'])
+@app.route('/cart', methods=['GET','POST'])
 def cart():
     return render_template('cart.html')
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['GET','POST'])
 def register():
     return render_template('register.html')
 
-@app.route('/contact-us', methods=['GET'])
+@app.route('/contact-us', methods=['GET','POST'])
 def contact():
     return render_template('contact.html')
 
-@app.route('/about', methods=['GET'])
+@app.route('/about', methods=['GET','POST'])
 def about():
     return render_template('about.html')
 
+@app.route('/payment', methods=['GET','POST'])
+def about():
+    return render_template('payment.html')
+
+@app.route('/checkout', methods=['GET','POST'])
+def about():
+    return render_template('checkout.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
