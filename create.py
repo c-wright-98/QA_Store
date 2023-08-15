67d5713b52865097a87706fe6f4b3331418745ff
c-wright-98 <corvus.wright@gmail.com>
2023-08-15 21:24:15 +0100

    Got products to show up on page :D

--- create.py
+++ create.py
@@ -10,7 +10,7 @@ outdoors = Category(
     name = 'Outdoors'
 )
 
-indoors = Category(
+pots = Category(
     category_id = 103,
     name = 'Pot'
 )
@@ -25,5 +25,22 @@ test_product = Products(
     stock = 99
 )
 
+test_product2 = Products(
+    product_id = 1002,
+    category_id = 102,
+    image_URL = 'static/images/plants/test-plant.png',
+    description = 'This is a test',
+    name = 'Test Product',
+    price = 9.99,
+    stock = 99
+)
+
+
 with app.app_context():
     db.create_all()
+    #db.session.add(indoors)
+    #db.session.add(outdoors)
+    #db.session.add(pots)
+    #db.session.add(test_product)
+    #db.session.add(test_product2)
+    db.session.commit()
