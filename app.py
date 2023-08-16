2762e60fdf0ca8255b8a7c873a89a6041ad6702c
c-wright-98 <corvus.wright@gmail.com>
2023-08-16 01:25:02 +0100

    Got tooltips to work, validators are now stopping submit

--- app.py
+++ app.py
@@ -56,23 +56,19 @@ class Orders(db.Model):
 class PaymentForm(FlaskForm):
     cardholder_name = StringField(
         "Cardholder Name",
-        validators=[DataRequired(), Length(min=2, max=30)],
-        render_kw={"title": "Enter the cardholder's name"}
+        validators=[DataRequired(), Length(min=2, max=30)]
         )
     cardnumber = StringField(
         "Card Number",
-        validators=[DataRequired(), Length(min=16, max=16)],
-        render_kw={"title": "Enter the 16 digit card number"}
+        validators=[DataRequired(), Length(min=16, max=16)]
         )
     expire = StringField(
         "Expire Date",
-        validators=[DataRequired(), Length(min=7, max=7)],
-        render_kw={"title": "Enter the expiry date in MM/YYYY format"}
+        validators=[DataRequired(), Length(min=7, max=7)]
         )
     SC = StringField(
         "Security Code (CVC)",
-        validators=[DataRequired(), Length(min=3, max=4)],
-        render_kw={"title": "Enter the Security Code (CVC)"}
+        validators=[DataRequired(), Length(min=3, max=4)]
         )
 
 
@@ -120,8 +116,10 @@ def about():
 @app.route('/payment', methods=['GET','POST'])
 def payment():
     form = PaymentForm()
+
     if form.validate_on_submit():
-        return redirect(url_for('success'))
+        return redirect(url_for('successful'))
+
     return render_template('payment.html')
 
 @app.route('/checkout', methods=['GET','POST'])
